fb1bbcc0e32b6043f0eca62eee85057c5a5444bd
Bas Pennings <baspster@gmail.com>
2017-01-13 20:56:07 +0100

    Tiny test cleanup

--- src/tokenizer_spec.ts
+++ src/tokenizer_spec.ts
@@ -1,19 +1,23 @@
 import { Tokenizer } from './tokenizer';
 
 describe('tokenizer', () => {
-   it('tokenizes on whitespace', () => {
-       const inp = 'foo bar\tquux\nfrotz\rbaz';
-       const exp = ['foo', 'bar', 'quux', 'frotz', 'baz'];
-       const sut = new Tokenizer();
-       const act = sut.tokenize(inp);
-       expect(act).toEqual(exp);
-   });
+    let sut: Tokenizer; 
+    
+    beforeEach(() => {
+        sut = new Tokenizer();
+    });
 
-   it('removes duplicate whitespace', () => {
-       const inp = 'foo  bar \tquux \n\tfrotz \r \n \tbaz';
-       const exp = ['foo', 'bar', 'quux', 'frotz', 'baz'];
-       const sut = new Tokenizer();
-       const act = sut.tokenize(inp);
-       expect(act).toEqual(exp);
-   });
+    it('tokenizes on whitespace', () => {
+        const inp = 'foo bar\tquux\nfrotz\rbaz';
+        const exp = ['foo', 'bar', 'quux', 'frotz', 'baz'];
+        const act = sut.tokenize(inp);
+        expect(act).toEqual(exp);
+    });
+
+    it('removes duplicate whitespace', () => {
+        const inp = 'foo  bar \tquux \n\tfrotz \r \n \tbaz';
+        const exp = ['foo', 'bar', 'quux', 'frotz', 'baz'];
+        const act = sut.tokenize(inp);
+        expect(act).toEqual(exp);
+    });
 });
